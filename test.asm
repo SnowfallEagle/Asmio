7ede77d1c13c550da76a8ebf81f79ec43b683e01
17seannnn <17sean@protonmail.com>
2022-06-07 22:40:53 +0300

    feat: add check string in test

--- test.asm
+++ test.asm
@@ -4,6 +4,19 @@
 global _start
 
 section .text
+;; Check char operations
 _start: getc                ; get character
         putc    al          ; put character
+
+;; Check string operations
+        sub     esp, 256    ; 256 byte string
+
+        mov     esi, esp    ; esp -> esi
+        read    stdin, esi, 256 ; read string
+        cmp     eax, EOF    ; if not EOF
+        je      .clean      ;   jump to clean
+        write   stdout, esi, eax    ; write what we`ve read
+
+.clean: add     esp, 256    ; free string
+
         exit    0           ; successful exit
