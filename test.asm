7a6835dd8422d49070f09403153e81b2d2684209
17seannnn <17sean@protonmail.com>
2022-06-07 22:02:53 +0300

    feat: exit macros

--- test.asm
+++ test.asm
@@ -6,7 +6,4 @@ global _start
 section .text
 _start: getc                ; get character
         putc    al          ; put character
-
-        mov     eax, 1      ; exit
-        mov     ebx, 0      ; status 0
-        int     0x80        ; syscall
+        exit    0           ; successful exit
