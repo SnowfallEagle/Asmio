48c8486b56a32cd2042779dfc7047fd38defdb98
17seannnn <17sean@protonmail.com>
2022-06-08 22:31:51 +0300

    fix: getargv return argv, not argv[0]

--- test.asm
+++ test.asm
@@ -8,27 +8,18 @@ global _start
 
 section .data
 fn      db "log", 0
-argcmsg db "argc: "
-argclen equ $-argcmsg
-argvmsg db "argv[0]: "
-argvlen equ $-argvmsg
 
 section .text
 _start:
-        getargc eax         ; argc -> eax
-        add     eax, "0"    ; make character from number (argc < 10)
-        sub     esp, 4      ; memory for buffer
-        mov     edi, esp    ; edi is buffer
-        mov     [edi], eax  ; eax -> [edi]
-        write   stdout, argcmsg, argclen    ; write argc msg
-        write   stdout, edi, 1  ; write character
-        add     esp, 4      ; free stack
-        putc    10          ; new line
+;; Show arguments count
+        getargc eax             ; argc -> eax
+        add     eax, "0"        ; make character from 1 number integer
+        putc    al              ; put character
+        putc    10              ; new line
 
-        getargv edi         ; argv -> edi
-        write   stdout, argvmsg, argvlen    ; write argv msg
-        putc    10          ; new line
-        ;puts    edi         ; put string in argv
+;; Show argv[0]
+        getargv edi             ; argv -> edi
+        puts    [edi]           ; put string from 1 arg
 
 ;; Check string operations
         sub     esp, 260    ; 256 byte string + 4 byte integer
