e947eae573f94b43487fee3e3df4fbb87b04c953
17seannnn <17sean@protonmail.com>
2022-06-08 15:20:43 +0300

    feat: getargc, getargv

--- test.asm
+++ test.asm
@@ -1,15 +1,34 @@
-; CDECL
+;;; License: LGPL-2.1
+;;; Copyright (C) 2022 Sergey Nikonov (17seannnn)
+;;; CDECL
+
 %include "asmio.inc"
 
 global _start
 
 section .data
 fn      db "log", 0
+argcmsg db "argc: "
+argclen equ $-argcmsg
+argvmsg db "argv[0]: "
+argvlen equ $-argvmsg
 
 section .text
-;; Check char operations
-_start: getc                ; get character
-        putc    al          ; put character
+_start:
+        getargc eax         ; getargc -> eax
+        add     eax, "0"    ; make character from number (argc < 10)
+        sub     esp, 4      ; memory for buffer
+        mov     edi, esp    ; edi is buffer
+        mov     [edi], eax  ; eax -> [edi]
+        write   stdout, argcmsg, argclen    ; write argc msg
+        write   stdout, edi, 1  ; write character
+        add     esp, 4      ; free stack
+        putc    10          ; new line
+        ;getargv edi
+        write   stdout, argvmsg, argvlen    ; write argv msg
+        ;strlen argv, eax
+        ;write  stdout, edi, eax
+        putc    10          ; new line
 
 ;; Check string operations
         sub     esp, 260    ; 256 byte string + 4 byte integer
