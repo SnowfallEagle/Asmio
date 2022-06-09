2d8f550ce6ee005a7622235b9cd2eca3788ab5f1
17seannnn <17sean@protonmail.com>
2022-06-09 14:34:35 +0300

    feat: vcall macros

--- test.asm
+++ test.asm
@@ -10,15 +10,24 @@ section .data
 fn      db "log", 0
 
 section .text
-_start:
-;; Show arguments count
-        getargc eax             ; argc -> eax
+test_vcall:
+        mov     eax, [esp+4]    ; get first argument
         add     eax, "0"        ; make character from 1 number integer
         putc    al              ; put character
         putc    10              ; new line
+        ret                     ; return
+
+_start:
+;; Show arguments count
+        getargc eax             ; argc -> eax
+        sub     esp, 4          ; local variable for argc
+        mov     edi, esp        ; edi = &argc
+        mov     [edi], eax      ; eax -> argc
+        vcall   test_vcall, eax ; test vcall macros
+        mov     eax, [edi]      ; get argc
+        add     esp, 4          ; free local variable
 
 ;; Show all arguments
-        sub     eax, "0"        ; get integer from character
         getargv edi             ; argv -> edi
 .lp:    mov     ebx, [edi]      ; argv string address -> ebx
         test    ebx, ebx        ; if ebx == 0
